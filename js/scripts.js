2ec44f9b25587ea5807841d50df033f354ba35a5
Tyson Lackey <lackeyt90@gmail.com>
2020-06-02 11:44:13 -0700

    add toggleclass logic to <p> to highlight/border on click
    Co-Authored-by: Kate Skorija <kateskorija@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
     $("body").addClass("darkmode");
     $("button#light").toggle();
     $("button#dark").toggle();
-  })
+  });
 
   $("button#light").click(function() {
     $("body").removeClass("darkmode");
     $("button#dark").toggle();
     $("button#light").toggle();
   });
-})
+
+  $("p").click(function() {
+    $(this).toggleClass("borderHighlight");
+  });
+});
