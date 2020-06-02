1bd691f63d690ab0298b305223c88d53fe796857
Tyson Lackey <lackeyt90@gmail.com>
2020-06-02 11:19:43 -0700

    add initial JS logic for button toggles and theme changing
    Co-Authored-by: Kate Skorija <kateskorija@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").addClass("darkmode");
+    $("button#light").toggle();
+    $("button#dark").toggle();
+  })
+
+  $("button#light").click(function() {
+    $("body").removeClass("darkmode");
+    $("button#dark").toggle();
+    $("button#light").toggle();
+  });
+})
